fa551a789d9e6cae7f861bc0db176ee097a66f38
gbiz0 <jardimbgustavo@gmail.com>
2024-10-06 23:31:44 -0300

    feat: add new components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,37 @@
-import './globals.css'
-import { Poppins } from 'next/font/google'
-import Header from '@/app/components/Header'
-import Footer from '@/app/components/Footer'
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] })
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata = {
-  title: 'Coding Ferpa',
-  description: 'Comunidade de tecnologia para jovens em Fernandópolis',
-}
+  title: "Coding Ferpa",
+  description: "Comunidade de tecnologia para jovens em Fernandópolis",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pt-BR">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/icon.png"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={`${poppins.className} flex flex-col min-h-screen`}>
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
       </body>
     </html>
-  )
+  );
 }
