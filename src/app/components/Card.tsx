5db8e510cd011328fdaafb580e11b119142f37f1
gbiz0 <jardimbgustavo@gmail.com>
2024-10-09 22:03:46 -0300

    feat: add style on cards

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 
 interface TalkCardProps {
-    title: string;
-    speaker: string;
-    desc: string;
+  title: string;
+  speaker: string;
+  desc: string;
 }
 
-const Card: React.FC<TalkCardProps> = ({title, speaker, desc}) => {
-    return (
-        <div className="w-1/4 h-[calc(100%)] bg-white">
-            <div className="card-title">
-                <h1 className="text-2xl"> {title} </h1>
-            </div>
-            <div className="card-content">
-                <p><strong>Palestrante:</strong> {speaker}</p>
-                <hr></hr>
-                <p> {desc}</p>
-            </div>
+const Card: React.FC<TalkCardProps> = ({ title, speaker, desc }) => {
+  return (
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+      <div className="bg-main-100 shadow-md rounded-lg overflow-hidden h-full flex flex-col transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+        <div className="card-title p-4 bg-main-100">
+          <h1 className="text-xl md:text-2xl font-bold mb-2 text-center text-white">
+            {title}
+          </h1>
         </div>
-
-    );
-}
+        <div className="card-content p-4 flex-grow">
+          <p className="mb-2 text-gray-200">
+            <strong>Palestrante:</strong> {speaker}
+          </p>
+          <hr className="mb-2 border-gray-400" />
+          <p className="text-gray-300">{desc}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
--- src/app/components/Talk.tsx
+++ src/app/components/Talk.tsx
@@ -1,45 +1,48 @@
-import Card from '@/app/components/Card';
-import data from '../assets/cards.json';
+import Card from "@/app/components/Card";
+import data from "../assets/cards.json";
 
 type Talk = {
-    id: number;
-    title: string;
-    speaker: string;
-    description: string;
+  id: number;
+  title: string;
+  speaker: string;
+  description: string;
 };
 
 export default function Talk() {
-    const talk_1 = data.talks.find((talk: Talk) => talk.id === 1);
-    const talk_2 = data.talks.find((talk: Talk) => talk.id === 2);
-    const talk_3 = data.talks.find((talk: Talk) => talk.id === 3);
+  const talk_1 = data.talks.find((talk: Talk) => talk.id === 1);
+  const talk_2 = data.talks.find((talk: Talk) => talk.id === 2);
+  const talk_3 = data.talks.find((talk: Talk) => talk.id === 3);
 
-    return (
-        <section id="talk" className="bg-black">
-            <div className="">
-                <div className="w-full flex justify-around md:pl-12 mt-8 md:mt-0 order-2 md:order-1">
-                    {talk_1 && (
-                        <Card 
-                            title={talk_1.title}
-                            speaker={talk_1.speaker}
-                            desc={talk_1.description}
-                        />
-                    )}
-                    {talk_2 && (
-                        <Card 
-                            title={talk_2.title}
-                            speaker={talk_2.speaker}
-                            desc={talk_2.description}
-                        />
-                    )}
-                    {talk_3 && (
-                        <Card 
-                            title={talk_3.title}
-                            speaker={talk_3.speaker}
-                            desc={talk_3.description}
-                        />
-                    )}
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section id="talk" className="bg-black text-white py-8">
+      <div className="text-center mb-8">
+        <h1 className="text-3xl font-bold">Palestras do dia 11/10</h1>
+      </div>
+      <div className=" mx-auto px-4">
+        <div className="flex flex-wrap justify-center gap-4">
+          {talk_1 && (
+            <Card
+              title={talk_1.title}
+              speaker={talk_1.speaker}
+              desc={talk_1.description}
+            />
+          )}
+          {talk_2 && (
+            <Card
+              title={talk_2.title}
+              speaker={talk_2.speaker}
+              desc={talk_2.description}
+            />
+          )}
+          {talk_3 && (
+            <Card
+              title={talk_3.title}
+              speaker={talk_3.speaker}
+              desc={talk_3.description}
+            />
+          )}
+        </div>
+      </div>
+    </section>
+  );
 }
